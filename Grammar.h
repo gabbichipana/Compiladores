2a216acc1341adf9096a3956012bc507b20423ed
gabbichipana <42560116+gabbichipana@users.noreply.github.com>
2021-06-27 23:49:14 -0500

    Add files via upload

---
+++ Grammar.h
@@ -0,0 +1,26 @@
+#include "Production.h"
+
+
+class Grammar
+{
+	private:
+		std::vector<Produccion> producciones;
+		std::vector<std::string> NoTerminales;
+		std::vector<std::string> Terminales;
+	public:
+		Grammar() = default;
+
+    void setProducciones(vector <Produccion> producciones)
+    {
+      this->producciones = producciones;
+    }
+    
+    std::vector<Produccion> getProducciones(std::string nt)
+    {
+      return this->producciones
+    }		
+
+    
+    std::vector<Produccion> getPrimeros(string nt);
+		std::vector<Produccion> getSiguientes(string nt);
+};
