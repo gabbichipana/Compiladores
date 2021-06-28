f38f6bb340c7fe7c547bd72a6a4a2503bd9101bc
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2021-06-27 23:52:29 -0500

    Create LL1.h

---
+++ LL1.h
@@ -0,0 +1,16 @@
+class LL1{
+  private:
+    //Gramatica
+    //Tas
+  public:
+    LL1 () = default
+    bool process(std::string)
+    {
+      //Accepted or not
+    }
+    void readGramatica(string txt)
+    {
+
+    }
+    
+}
